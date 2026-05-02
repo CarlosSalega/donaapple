2e29ecd93235e3631441870f5a85c031817c92c3
CarlosSalega <carlosalfredosalega@gmail.com>
2026-05-02 10:30:08 -0300

    fix: update Header import path to correct location

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { HeroSection, FeaturedProductsSection, Footer } from "@/features/home";
 import { TestimonialsSection } from "@/features/social/components/TestimonialsSection";
 import { FinalCTA } from "@/features/social/components/FinalCTA";
 import { StoreInfoSection } from "@/features/store/components/StoreInfoSection";
-import { Header } from "@/features/layout/components/Header";
+import { Header } from "@/features/home/Header";
 import { UrgencyBanner } from "@/features/home/UrgencyBanner";
 
 export default function Home() {
