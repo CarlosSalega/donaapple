08e8d1b4d72c0131bfdc0a3dbb5696d50edb7d0d
CarlosSalega <carlosalfredosalega@gmail.com>
2026-05-10 12:30:50 -0300

    feat: export LandingSection component from ui index

--- src/shared/components/ui/index.ts
+++ src/shared/components/ui/index.ts
@@ -3,4 +3,5 @@ export * from "./apple-icon";
 export * from "./chevron-left";
 export * from "./chevron-right";
 export * from "./instagram-icon";
+export * from "./LandingSection";
 export * from "./whatsapp-icon";
